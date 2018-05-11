8cfb3ce21d4885289b972283b3ceb9685c4207c4
Clayton Fidelis <clayton.fidelis@live.com>
2018-05-11 13:12:03 -0300

    feat(quiz): add animations to feedback

--- src/Answer.js
+++ src/Answer.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Motion, spring } from 'react-motion'
 
 import errorIcon from './icons/error.svg'
 import successIcon from './icons/success.svg'
 import './styles/answer.css'
 
 const Answer = props => {
+  const errorSpringConfig = {
+    stiffness: 500,
+    damping: 15
+  }
+
   return (
     <div
       className="answer"
@@ -14,21 +20,51 @@ const Answer = props => {
       <div className="answer__identifier">{ props.identifier }</div>
       <div className="answer__value">{ props.company }</div>
 
-      { props.error && (
-        <img
-          className="answer__feedback"
-          src={errorIcon}
-          alt="Error icon"
-        />
-      )}
+      <Motion
+        defaultStyle={{
+          scale: 0,
+          rotate: 90
+        }}
+        style={{
+          scale: spring(props.error ? 1 : 0, errorSpringConfig),
+          rotate: spring(props.error ? 0 : 90, errorSpringConfig)
+        }}
+      >
+        {({ scale, rotate }) => (
+          <img
+            className="answer__feedback"
+            src={errorIcon}
+            alt="Error icon"
+            style={{
+              transform: `scale(${scale}) rotateZ(${rotate}deg)`,
+              WebkitTransform: `scale(${scale}deg)`
+            }}
+          />
+        )}
+      </Motion>
 
-      { props.success && (
-        <img
-          className="answer__feedback"
-          src={successIcon}
-          alt="Success icon"
-        />
-      )}
+      <Motion
+        defaultStyle={{
+          scale: 0,
+          rotate: 90
+        }}
+        style={{
+          scale: spring(props.success ? 1 : 0, errorSpringConfig),
+          rotate: spring(props.success ? 0 : 90, errorSpringConfig)
+        }}
+      >
+        {({ scale, rotate }) => (
+          <img
+            className="answer__feedback"
+            src={successIcon}
+            alt="Success icon"
+            style={{
+              transform: `scale(${scale}) rotateZ(${rotate}deg)`,
+              WebkitTransform: `scale(${scale}deg)`
+            }}
+          />
+        )}
+      </Motion>
     </div>
   )
 }
